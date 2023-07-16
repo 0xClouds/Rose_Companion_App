75c998d4e4001906e2b89c854b594cc5f4d92a21
xiuxiu62 <jacremer@live.com>
2023-07-16 10:33:28 -0700

    try whisper

--- src/components/AudioRecorder.tsx
+++ src/components/AudioRecorder.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, MutableRefObject, SetStateAction, useEffect } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import { AppState } from './QAModal';
 import "./AudioRecorder.css";
 
@@ -9,40 +9,71 @@ export interface AudioRecorderProps {
     formReference: MutableRefObject<HTMLFormElement | undefined> 
 }
 
-export default function AudioRecorder({appState, setDialog, formReference}: AudioRecorderProps) {
+// function AudioRecorderTemp({appState, setDialog, formReference}: AudioRecorderProps) {
+//   const {
+//     transcript,
+//     listening,
+//     resetTranscript,
+//     browserSupportsSpeechRecognition
+//   } = useSpeechRecognition();
+
+//   if (!browserSupportsSpeechRecognition) 
+//     return <span>Browser doesn't support speech recognition.</span>;
+//   
+
+//   const submit = () => {
+//     setDialog(transcript);
+//     // if (formReference.current)  
+//     //   formReference.current.dispatchEvent(new Event("submit", { cancelable: true, bubbles: true }));
+
+//     SpeechRecognition.stopListening();
+//     resetTranscript();
+//   };
+
+//   useEffect(async () => {
+//     await SpeechRecognition.startListening();
+//   }, []); 
+
+//   return (
+//     <div className="container">
+//       { listening ?  
+//           <span className="microphone-on" /> :
+//           <span className="microphone-off" /> 
+//       }
+//       <button className="button" onClick={submit}>Submit</button>
+//       <button onClick={SpeechRecognition.startListening}>Start</button>
+//       <p>{transcript}</p>
+//     </div>
+//   );
+// };
+
+
+import { useWhisper } from '@chengsokdara/use-whisper'
+
+function AudioRecorder({appState, setDialog, formReference}: AudioRecorderProps) {
   const {
+    recording,
+    speaking,
     transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
-
-  if (!browserSupportsSpeechRecognition) 
-    return <span>Browser doesn't support speech recognition.</span>;
-  
-
-  const submit = () => {
-    setDialog(transcript);
-    if (formReference.current)  
-      formReference.current.dispatchEvent(new Event("submit", { cancelable: true, bubbles: true }));
-
-    SpeechRecognition.stopListening();
-    resetTranscript();
-  };
-
-  useEffect(() => {
-    SpeechRecognition.startListening();
-  }, []); 
+    transcribing,
+    pauseRecording,
+    startRecording,
+    stopRecording,
+  } = useWhisper({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, // YOUR_OPEN_AI_TOKEN
+    autoStart: true,
+  });
 
   return (
-    <div className="container">
-      { listening ?  
-          <span className="microphone-on" /> :
-          <span className="microphone-off" /> 
-      }
-      <button className="button" onClick={submit}>Submit</button>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <p>{transcript}</p>
+    <div>
+      <p>Recording: {recording}</p>
+      <p>Speaking: {speaking}</p>
+      <p>Transcribed Text: { transcript ? transcript.text : "" }</p>
+      <button onClick={() => startRecording()}>Start</button>
+      <button onClick={() => pauseRecording()}>Pause</button>
+      <button onClick={() => stopRecording()}>Stop</button>
     </div>
   );
-};
+}
+
+export default AudioRecorder;
