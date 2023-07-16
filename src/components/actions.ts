25b3d4de40a9223374018ebe402b76105b217de4
Cloud <cloud.frontier0@gmail.com>
2023-07-16 14:57:13 -0700

    hardcode this

--- src/components/actions.ts
+++ src/components/actions.ts
@@ -5,7 +5,16 @@ import dotenv from "dotenv";
 import { parse } from "path";
 
 export async function getCompanions() {
-  const COMPFILE = "../../companions/companions.json";
+  const COMPFILE = [
+    {
+      name: "Rosie",
+      title: "San Diego native, and fluent in spanish. I am your AI tutor",
+      imageUrl: "/rosie_ai.png",
+      llm: "chatgpt",
+      phone: "OPTIONAL_COMPANION_PHONE_NUMBER",
+    },
+  ];
+
   var companions = [];
   console.log("Loading companion descriptions from " + COMPFILE);
   var fs = require("fs");
